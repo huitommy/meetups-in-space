575dcd549e4b346351807be73dff9ac2923242c4
Tommy Hui <hui.1tommy@gmail.com>
2016-03-16 14:16:25 -0400

    cleaned up code

--- spec/features/04_user_sees_attendants_in_meetup_page_spec.rb
+++ spec/features/04_user_sees_attendants_in_meetup_page_spec.rb
@@ -4,47 +4,46 @@ require 'pry'
 feature "meetup's show page" do
   let(:user1) do
     User.create(
-      provider: "github",
-      uid: "1",
-      username: "jarlax1",
-      email: "jarlax1@launchacademy.com",
-      avatar_url: "https://avatars2.githubusercontent.com/u/174825?v=3&s=400"
+      provider: 'github',
+      uid: '1',
+      username: 'jarlax1',
+      email: 'jarlax1@launchacademy.com',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/174825?v=3&s=400'
     )
   end
 
   let(:user2) do
     User.create(
-      provider: "github",
-      uid: "2",
-      username: "jarlax2",
-      email: "jarlax2@launchacademy.com",
-      avatar_url: "https://avatars2.githubusercontent.com/u/174825?v=3&s=400"
+      provider: 'github',
+      uid: '2',
+      username: 'jarlax2',
+      email: 'jarlax2@launchacademy.com',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/174825?v=3&s=400'
     )
   end
 
   let(:user3) do
     User.create(
-      provider: "github",
-      uid: "3",
-      username: "jarlax3",
-      email: "jarlax3@launchacademy.com",
-      avatar_url: "https://avatars2.githubusercontent.com/u/174825?v=3&s=400"
+      provider: 'github',
+      uid: '3',
+      username: 'jarlax3',
+      email: 'jarlax3@launchacademy.com',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/174825?v=3&s=400'
     )
   end
 
-  scenario "views meetup attendees" do
-    meetup1 = Meetup.create(name: "Ruby Hack Night", description: "Learn to Hack with Ruby", location: "Jupiter", user_id: user1.id)
+  scenario 'views meetup attendees' do
+    meetup1 = Meetup.create(name: 'Ruby Hack Night', description: 'Learn to Hack with Ruby', location: 'Jupiter', user_id: user1.id)
     MeetupUser.create(user_id: user1.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
     MeetupUser.create(user_id: user2.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
     MeetupUser.create(user_id: user3.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
 
-    visit "/meetups"
-    click_link "Ruby Hack Night"
+    visit '/meetups'
+    click_link 'Ruby Hack Night'
 
-    expect(page).to have_content("jarlax1")
-    expect(page).to have_content("jarlax2")
-    expect(page).to have_content("jarlax3")
+    expect(page).to have_content('jarlax1')
+    expect(page).to have_content('jarlax2')
+    expect(page).to have_content('jarlax3')
     expect(page).to have_xpath("//img[@src=\"https://avatars2.githubusercontent.com/u/174825?v=3&s=400\"]")
   end
-
 end
--- spec/features/05_user_joins_meetups_spec.rb
+++ spec/features/05_user_joins_meetups_spec.rb
@@ -3,68 +3,66 @@ require 'pry'
 
 require 'spec_helper'
 
-feature "join meetup" do
+feature 'join meetup' do
   let(:user1) do
     User.create(
-      provider: "github",
-      uid: "1",
-      username: "jarlax1",
-      email: "jarlax1@launchacademy.com",
-      avatar_url: "https://avatars2.githubusercontent.com/u/174825?v=3&s=400"
+      provider: 'github',
+      uid: '1',
+      username: 'jarlax1',
+      email: 'jarlax1@launchacademy.com',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/174825?v=3&s=400'
     )
   end
 
   let(:user2) do
     User.create(
-      provider: "github",
-      uid: "2",
-      username: "jarlax2",
-      email: "jarlax2@launchacademy.com",
-      avatar_url: "https://avatars2.githubusercontent.com/u/174825?v=3&s=400"
+      provider: 'github',
+      uid: '2',
+      username: 'jarlax2',
+      email: 'jarlax2@launchacademy.com',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/174825?v=3&s=400'
     )
   end
 
-  scenario "successfully joins meetup" do
-    meetup1 = Meetup.create(name: "Ruby Hack Night", description: "Learn to Hack with Ruby", location: "Jupiter", user_id: user1.id)
+  scenario 'successfully joins meetup' do
+    meetup1 = Meetup.create(name: 'Ruby Hack Night', description: 'Learn to Hack with Ruby', location: 'Jupiter', user_id: user1.id)
     MeetupUser.create(user_id: user1.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
 
-    visit "/meetups"
+    visit '/meetups'
     sign_in_as user2
-    click_link "Ruby Hack Night"
+    click_link 'Ruby Hack Night'
 
     expect(page).to have_selector("input[type=submit][value='Join']")
 
-    click_button "Join"
+    click_button 'Join'
 
-    expect(page).to have_content("You have been added to this meetup.")
-    expect(page).to have_content("jarlax2")
+    expect(page).to have_content('You have joined the meetup.')
+    expect(page).to have_content('jarlax2')
   end
 
-  scenario "tries to join meetup not signed in" do
-    meetup1 = Meetup.create(name: "Ruby Hack Night", description: "Learn to Hack with Ruby", location: "Jupiter", user_id: user1.id)
+  scenario 'tries to join meetup not signed in' do
+    meetup1 = Meetup.create(name: 'Ruby Hack Night', description: 'Learn to Hack with Ruby', location: 'Jupiter', user_id: user1.id)
     MeetupUser.create(user_id: user1.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
 
-    visit "/meetups"
-    click_link "Ruby Hack Night"
+    visit '/meetups'
+    click_link 'Ruby Hack Night'
 
     expect(page).to have_selector("input[type=submit][value='Join']")
 
-    click_button "Join"
+    click_button 'Join'
 
-    expect(page).to have_content("Please sign in to join a meetup.")
+    expect(page).to have_content('Please sign in before joining meetups')
   end
 
-  scenario "visits show page signed in and already member of event" do
-    meetup1 = Meetup.create(name: "Ruby Hack Night", description: "Learn to Hack with Ruby", location: "Jupiter", user_id: user1.id)
+  scenario 'visits show page signed in and already member of event' do
+    meetup1 = Meetup.create(name: 'Ruby Hack Night', description: 'Learn to Hack with Ruby', location: 'Jupiter', user_id: user1.id)
     MeetupUser.create(user_id: user1.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
     MeetupUser.create(user_id: user2.id, meetup_id: meetup1.id, meetup_creator_id: meetup1.user_id)
 
-    visit "/meetups"
+    visit '/meetups'
     sign_in_as user2
-    click_link "Ruby Hack Night"
+    click_link 'Ruby Hack Night'
 
     expect(page).to have_no_selector("input[type=submit][value='Join']")
   end
-
-
 end
